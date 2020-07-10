3a9590b56a992af3027a33172bf9b5e2347de324
adz-313 <ads73818@gmail.com>
2020-07-10 19:25:17 +0530

    Added flat bottom button

--- lib/screens/authentication/sign_in.dart
+++ lib/screens/authentication/sign_in.dart
@@ -3,6 +3,34 @@ import 'package:i_scanner/screens/home/shared/loading.dart';
 import 'package:i_scanner/services/auth.dart';
 import 'package:i_scanner/screens/home/shared/constants.dart';
 
+class BottomButton extends StatelessWidget {
+  final AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: FlatButton.icon(
+        onPressed: () async {
+          dynamic result = await _auth.signInAnon();
+          if (result == null) {
+            print('error signing in');
+          } else {
+            print('sign in successful');
+            print(result.uid);
+          }
+        },
+        icon: Icon(
+          Icons.arrow_forward,
+          color: Colors.red,
+        ),
+        label: Text('Skip sign-in',
+            style: TextStyle(fontSize: 15, color: Colors.redAccent)),
+      ),
+    );
+  }
+}
+
 class SignIn extends StatefulWidget {
   final Function toggleView;
   SignIn({this.toggleView});
@@ -30,7 +58,7 @@ class _SignInState extends State<SignIn> {
             appBar: AppBar(
               backgroundColor: Colors.redAccent,
               elevation: 0.0,
-              title: Text('Sign in'),
+              title: Text('Sign in to IScanner'),
               actions: <Widget>[
                 FlatButton.icon(
                   icon: Icon(Icons.person),
@@ -39,78 +67,67 @@ class _SignInState extends State<SignIn> {
                     widget.toggleView();
                   },
                 ),
-                FlatButton.icon(
-                  icon: Icon(Icons.arrow_forward),
-                  label: Text('Sign in anon'),
-                  onPressed: () async {
-                    dynamic result = await _auth.signInAnon();
-                    if (result == null) {
-                      print('error signing in');
-                    } else {
-                      print('sign in successful');
-                      print(result.uid);
-                    }
-                  },
-                ),
               ],
             ),
             body: Container(
-                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(height: 20.0),
-                      TextFormField(
-                          decoration:
-                              textInputDecoration.copyWith(hintText: 'Email'),
-                          validator: (val) =>
-                              val.isEmpty ? 'Enter an email' : null,
-                          onChanged: (val) {
-                            setState(() => email = val);
-                          }),
-                      SizedBox(height: 20.0),
-                      TextFormField(
-                          decoration: textInputDecoration.copyWith(
-                              hintText: 'Password'),
-                          obscureText: true,
-                          validator: (val) => val.length < 6
-                              ? 'Enter a password 6+ chars long'
-                              : null,
-                          onChanged: (val) {
-                            setState(() => password = val);
-                          }),
-                      SizedBox(height: 20.0),
-                      RaisedButton(
-                        color: Colors.red[300],
-                        child: Text(
-                          'Sign in',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                        decoration:
+                            textInputDecoration.copyWith(hintText: 'Email'),
+                        validator: (val) =>
+                            val.isEmpty ? 'Enter an email' : null,
+                        onChanged: (val) {
+                          setState(() => email = val);
+                        }),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                        decoration:
+                            textInputDecoration.copyWith(hintText: 'Password'),
+                        obscureText: true,
+                        validator: (val) => val.length < 6
+                            ? 'Enter a password 6+ chars long'
+                            : null,
+                        onChanged: (val) {
+                          setState(() => password = val);
+                        }),
+                    SizedBox(height: 20.0),
+                    RaisedButton(
+                      color: Colors.red[300],
+                      child: Text(
+                        'Sign in',
+                        style: TextStyle(
+                          color: Colors.white,
                         ),
-                        onPressed: () async {
-                          if (_formKey.currentState.validate()) {
-                            setState(() => loading = true);
-                            dynamic result = await _auth
-                                .signInWithEmailAndPassword(email, password);
-                            if (result == null) {
-                              setState(() {
-                                error = 'Invalid credentials';
-                                loading = false;
-                              });
-                            }
+                      ),
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          setState(() => loading = true);
+                          dynamic result = await _auth
+                              .signInWithEmailAndPassword(email, password);
+                          if (result == null) {
+                            setState(() {
+                              error = 'Invalid credentials';
+                              loading = false;
+                            });
                           }
-                        },
-                      ),
-                      SizedBox(height: 12.0),
-                      Text(
-                        error,
-                        style: TextStyle(color: Colors.red, fontSize: 14.0),
-                      ),
-                    ],
-                  ),
-                )),
+                        }
+                      },
+                    ),
+                    SizedBox(height: 12.0),
+                    Text(
+                      error,
+                      style: TextStyle(color: Colors.red, fontSize: 14.0),
+                    ),
+                    Expanded(child: BottomButton()),
+                  ],
+                ),
+              ),
+            ),
           );
   }
 }
